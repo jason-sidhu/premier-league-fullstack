41b6c4ba99f1ab325a1eb107c8842f887afbd868
jason-sidhu <sidhu.jason03@Gmail.com>
2023-09-10 14:33:15 -0400

    deploy backend

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ import SignUpPage from './Pages/SignUpPage';
 import Stats from './Pages/stats';
 import Profile from './Pages/profile';
 import Favourite from './Pages/favourite';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-
-
+if(process.env.NODE_ENV === 'production') {disableReactDevTools()};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
