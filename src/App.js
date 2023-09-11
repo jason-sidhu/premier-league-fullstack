f9e56a019dcc2b236a3b567f6249357b249a9497
jason-sidhu <sidhu.jason03@Gmail.com>
2023-09-11 01:03:18 -0400

    use a app.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Standings from './Pages/standings';
+import Fantasy from './Pages/fantasy';
+import Scores from './Pages/Scores';
+import Home from './Pages/home';
+import SignInOptionsPage from './Pages/SignInOptionsPage';
+import SignInPage from './Pages/SignInPage';
+import SignUpPage from './Pages/SignUpPage';
+import Stats from './Pages/stats';
+import Profile from './Pages/profile';
+import Favourite from './Pages/favourite';
+
+function App() {
+    return (
+<Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="Pages/scores" element={<Scores />} />
+        <Route path="Pages/standings" element={<Standings />} />
+        <Route path="Pages/stats" element={<Stats />} />
+        <Route path="Pages/fantasy" element={<Fantasy />} />
+        <Route path="Pages/favourite" element={<Favourite />} />
+        <Route path="sign-in-options" element={<SignInOptionsPage />} />
+        <Route path="Pages/signin" element={<SignInPage />} />
+        <Route path="Pages/signup" element={<SignUpPage />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+
+
+
+    );
+}
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Standings from './Pages/standings';
-import Fantasy from './Pages/fantasy';
-import Scores from './Pages/Scores';
-import Home from './Pages/home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App';
 import BSNavbar from './Components/Navbar/Navbar';
-import SignInOptionsPage from './Pages/SignInOptionsPage';
-import SignInPage from './Pages/SignInPage';
-import SignUpPage from './Pages/SignUpPage';
-import Stats from './Pages/stats';
-import Profile from './Pages/profile';
-import Favourite from './Pages/favourite';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
   <header>
     <BSNavbar/>
   </header>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="Pages/scores" element={<Scores />} />
-        <Route path="Pages/standings" element={<Standings />} />
-        <Route path="Pages/stats" element={<Stats />} />
-        <Route path="Pages/fantasy" element={<Fantasy />} />
-        <Route path="Pages/favourite" element={<Favourite />} />
-        <Route path="sign-in-options" element={<SignInOptionsPage />} />
-        <Route path="Pages/signin" element={<SignInPage />} />
-        <Route path="Pages/signup" element={<SignUpPage />} />
-        <Route path="profile" element={<Profile />} />
-      </Routes>
-    </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App/>} />
+    </Routes>
+  </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
